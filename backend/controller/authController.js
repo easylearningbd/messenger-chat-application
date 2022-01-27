36cabe8cfc6b35219d7ce2a93f970acac8a74718
easylearningbd <elebdsoft@gmail.com>
2022-01-28 01:15:28 +0600

    20  User Register complete Back End Part 5

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,5 +1,6 @@
 const formidable = require('formidable');
 const validator = require('validator');
+const registerModel = require('../models/authModel');
 
 
 module.exports.userRegister = (req, res) => {
@@ -7,45 +8,74 @@ module.exports.userRegister = (req, res) => {
      const form = formidable();
      form.parse(req, async (err, fields, files) => {
 
-          const {
-               userName, email, password,confirmPassword
-          } = fields;
+     const {
+          userName, email, password,confirmPassword
+     } = fields;
 
-          const {image} = files;
-          const error = [];
+     const {image} = files;
+     const error = [];
 
-          if(!userName){
-               error.push('Please provide your user name');
-          }
-          if(!email){
-               error.push('Please provide your Email');
-          }
-          if(email && !validator.isEmail(email)){
-               error.push('Please provide your Valid Email');
-          }
-          if(!password){
-               error.push('Please provide your Password');
-          }
-          if(!confirmPassword){
-               error.push('Please provide your confirm Password');
-          }
-          if(password && confirmPassword && password !== confirmPassword){
-               error.push('Your Password and Confirm Password not same');
-          }
-          if(password && password.length < 6){
-               error.push('Please provide password mush be 6 charecter');
-          }
-          if(Object.keys(files).length === 0){
-               error.push('Please provide user image');
-          }
-          if(error.length > 0){
-               res.status(400).json({
-                    error:{
-                         errorMessage : error
+     if(!userName){
+          error.push('Please provide your user name');
+     }
+     if(!email){
+          error.push('Please provide your Email');
+     }
+     if(email && !validator.isEmail(email)){
+          error.push('Please provide your Valid Email');
+     }
+     if(!password){
+          error.push('Please provide your Password');
+     }
+     if(!confirmPassword){
+          error.push('Please provide your confirm Password');
+     }
+     if(password && confirmPassword && password !== confirmPassword){
+          error.push('Your Password and Confirm Password not same');
+     }
+     if(password && password.length < 6){
+          error.push('Please provide password mush be 6 charecter');
+     }
+     if(Object.keys(files).length === 0){
+          error.push('Please provide user image');
+     }
+     if(error.length > 0){
+          res.status(400).json({
+               error:{
+                    errorMessage : error
+               }
+          })
+     } else {
+          const getImageName = files.image.originalFilename;
+          const randNumber = Math.floor(Math.random() * 99999 );
+          const newImageName = randNumber + getImageName;
+          files.image.originalFilename = newImageName;
+
+          const newPath = __dirname + `../../../frontend/public/image/${files.image.originalFilename}`;
+
+          try {
+               const checkUser = await registerModel.findOne({
+                    email:email
+               });
+               if(checkUser) {
+                    res.status(404).json({
+                         error: {
+                              errorMessage : ['Your email already exited']
+                         }
+                    })
+               }
+
+          } catch (error) {
+               res.status(500).json({
+                    error: {
+                         errorMessage : ['Interanl Server Error']
                     }
                })
-          } else {
 
+          }
+
+
+               
           }
  
 
