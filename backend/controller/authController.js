7acfb457e6881063fc839bb78a6ed9e43292ce1d
easylearningbd <elebdsoft@gmail.com>
2022-01-28 02:57:08 +0600

    22  User Register complete Back End Part 7

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -3,6 +3,8 @@ const validator = require('validator');
 const registerModel = require('../models/authModel');
 const fs = require('fs');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { options } = require('../routes/authRoute');
 
 module.exports.userRegister = (req, res) => {
 
@@ -72,8 +74,31 @@ module.exports.userRegister = (req, res) => {
                               email,
                               password : await bcrypt.hash(password,10),
                               image: files.image.originalFilename
+                         });
+
+                         const token = jwt.sign({
+                              id : userCreate._id,
+                              email: userCreate.email,
+                              userName: userCreate.userName,
+                              image: userCreate.image,
+                              registerTime : userCreate.createdAt
+                         }, process.env.SECRET,{
+                              expiresIn: process.env.TOKEN_EXP
+                         }); 
+
+const options = { expires : new Date(Date.now() + process.env.COOKIE_EXP * 24 * 60 * 60 * 1000 )}
+
+     res.status(201).cookie('authToken',token, options).json({
+          successMessage : 'Your Register Successful',token
+     })
+
+                          
+                    } else {
+                         res.status(500).json({
+                              error: {
+                                   errorMessage : ['Interanl Server Error']
+                              }
                          })
-                         console.log('registration Complete successfully')
                     }
                })
           }
@@ -85,8 +110,7 @@ module.exports.userRegister = (req, res) => {
                }
           })
 
-          }
-
+           } 
 
                
           }
