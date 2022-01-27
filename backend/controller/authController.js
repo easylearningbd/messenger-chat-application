24f9a1426be07d8c05fa3645e13f7e3109f2e85d
easylearningbd <elebdsoft@gmail.com>
2022-01-28 00:48:11 +0600

    19  User Register complete Back End Part 4

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,3 +1,56 @@
+const formidable = require('formidable');
+const validator = require('validator');
+
+
 module.exports.userRegister = (req, res) => {
-     console.log('register is working');
+
+     const form = formidable();
+     form.parse(req, async (err, fields, files) => {
+
+          const {
+               userName, email, password,confirmPassword
+          } = fields;
+
+          const {image} = files;
+          const error = [];
+
+          if(!userName){
+               error.push('Please provide your user name');
+          }
+          if(!email){
+               error.push('Please provide your Email');
+          }
+          if(email && !validator.isEmail(email)){
+               error.push('Please provide your Valid Email');
+          }
+          if(!password){
+               error.push('Please provide your Password');
+          }
+          if(!confirmPassword){
+               error.push('Please provide your confirm Password');
+          }
+          if(password && confirmPassword && password !== confirmPassword){
+               error.push('Your Password and Confirm Password not same');
+          }
+          if(password && password.length < 6){
+               error.push('Please provide password mush be 6 charecter');
+          }
+          if(Object.keys(files).length === 0){
+               error.push('Please provide user image');
+          }
+          if(error.length > 0){
+               res.status(400).json({
+                    error:{
+                         errorMessage : error
+                    }
+               })
+          } else {
+
+          }
+ 
+
+
+          
+     }) // end Formidable  
+    
 }
