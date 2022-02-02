fd0ea0085fd08fde590187aebfc7d70960eb4684
easylearningbd <elebdsoft@gmail.com>
2022-02-03 05:27:13 +0600

    52 Send Message Friends Part 4

--- backend/controller/messengerController.js
+++ backend/controller/messengerController.js
@@ -1,4 +1,5 @@
 const User = require('../models/authModel');
+const messageModel = require('../models/messageModel');
 
 module.exports.getFriends = async (req, res) => {
      const myId = req.myId;
@@ -19,8 +20,35 @@ module.exports.getFriends = async (req, res) => {
 
 module.exports.messageUploadDB = async (req, res) =>{
 
+     const {
+          senderName,
+          reseverId,
+          message
+     } = req.body
      const senderId = req.myId;
+
+     try{
+          const insertMessage = await messageModel.create({
+               senderId : senderId,
+               senderName : senderName,
+               reseverId : reseverId,
+               message : {
+                    text: message,
+                    image : ''
+               }
+          })
+          res.status(201).json({
+               success : true,
+               message: insertMessage
+          })
+
+     }catch (error){
+          res.status(500).json({
+               error: {
+                    errorMessage : 'Internal Sever Error'
+               }
+          })
+     }
+
      
-     console.log(senderId)
-     console.log(req.body);
 }
