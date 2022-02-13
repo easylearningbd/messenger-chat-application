71c21ab1519e0d7c41b5136e41d5d3c216337a20
easylearningbd <elebdsoft@gmail.com>
2022-02-14 02:00:41 +0600

    65  Active User Real Time Socket Part 2

--- socket/socket.js
+++ socket/socket.js
@@ -5,6 +5,19 @@ const io = require('socket.io')(8000,{
      }
 })
 
+let users = [];
+const addUser = (userId,socketId,userInfo) => {
+     const checkUser = users.some(u=> u.userId === userId );
+
+     if(!checkUser){
+          users.push({userId,socketId,userInfo});
+     }
+}
+
 io.on('connection',(socket)=>{
      console.log('Socket is connecting...')
+     socket.on('addUser',(userId,userInfo)=>{
+          addUser(userId,socket.id,userInfo);
+          io.emit('getUser',users);
+     })
 })
