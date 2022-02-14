7b5b28c63e23be419e8d25801ebe8faad4cafb1b
easylearningbd <elebdsoft@gmail.com>
2022-02-15 01:04:14 +0600

    69 Message Send Real Time Socket Part 1

--- socket/socket.js
+++ socket/socket.js
@@ -17,12 +17,24 @@ const userRemove = (socketId) => {
      users = users.filter(u=>u.socketId !== socketId );
 }
 
+const findFriend = (id) => {
+     return users.find(u=>u.userId === id);
+}
+
+
 io.on('connection',(socket)=>{
      console.log('Socket is connecting...')
      socket.on('addUser',(userId,userInfo)=>{
           addUser(userId,socket.id,userInfo);
           io.emit('getUser',users);
+     });
+     socket.on('sendMessage',(data)=>{
+          const user = findFriend(data.reseverId);
+          console.log(user);
      })
+
+
+
      socket.on('disconnect',() =>{
           console.log('user is disconnect... ');
           userRemove(socket.id);
