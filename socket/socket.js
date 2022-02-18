6162ef73f548c13711d7075763c11a7b3251790b
easylearningbd <elebdsoft@gmail.com>
2022-02-19 03:44:12 +0600

    85-Real Time Message Socket Part 2

--- socket/socket.js
+++ socket/socket.js
@@ -33,9 +33,24 @@ io.on('connection',(socket)=>{
           
           if(user !== undefined){
                socket.to(user.socketId).emit('getMessage', data)
-          }
-          
+          }          
      })
+
+     socket.on('messageSeen',msg =>{
+          const user = findFriend(msg.senderId);          
+          if(user !== undefined){
+               socket.to(user.socketId).emit('msgSeenResponse', msg)
+          }          
+     })
+
+     socket.on('delivaredMessage',msg =>{
+          const user = findFriend(msg.senderId);          
+          if(user !== undefined){
+               socket.to(user.socketId).emit('msgDelivaredResponse', msg)
+          }          
+     })
+
+
 
      socket.on('typingMessage',(data)=>{
           const user = findFriend(data.reseverId);
